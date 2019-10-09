6b39e97d5bda8726b55e7478b82fbb482775ea34
Brian Pompey <brianthescholar@gmail.com>
2019-10-09 01:15:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,8 +11,9 @@ class Triangle
     if (side_a <= 0) || (side_b <= 0) || (side_c <= 0)
       raise TriangleError
     elsif (side_a +side_b <= side_c) || (side_a +side_c <= side_b) || (side_c +side_b <= side_a)
-      raise TriangleError      
-    elsif (side_a == side_b) && (side_b == side_c)
+      raise TriangleError
+    else
+      if (side_a == side_b) && (side_b == side_c)
       :equilateral
     elsif (side_b == side_c) || (side_a == side_b) || (side_a == side_c)
       :isosceles
