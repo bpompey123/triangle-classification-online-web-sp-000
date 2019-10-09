e3768e326bfaac4c41ba574a4a0b755a40a2982a
Brian Pompey <brianthescholar@gmail.com>
2019-10-09 01:17:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,11 +14,12 @@ class Triangle
       raise TriangleError
     else
       if (side_a == side_b) && (side_b == side_c)
-      :equilateral
+        :equilateral
       elsif (side_b == side_c) || (side_a == side_b) || (side_a == side_c)
-      :isosceles
+        :isosceles
       elsif (side_a != side_b) && (side_b != side_c) && (side_a != side_c)
-      :scalene
+        :scalene
+      end
     end
 
   end
