cd994781ece0e1889a33ac6dbd69f238c950ed40
Brian Pompey <brianthescholar@gmail.com>
2019-10-09 01:06:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,8 @@ class Triangle
   end
 
   def kind
-    if (side_a == side_b) && (side_b == side_c)
+    if
+    elsif (side_a == side_b) && (side_b == side_c)
       :equilateral
     elsif (side_b == side_c) || (side_a == side_b) || (side_a == side_c)
       :isosceles
